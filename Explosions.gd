bdbe2c024ded1749203729c1008493e873c07a63
mahanen15 <mahanen@iu.edu>
2022-12-10 19:07:39 -0500

    EXERCISE COMPLETE

--- Explosions.gd
+++ Explosions.gd
@@ -1,16 +1,9 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite3D.play()
+	$Particles.emitting = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(_delta):
+	if not $AnimatedSprite3D.playing and not $Particles.emitting:
+		queue_free()
